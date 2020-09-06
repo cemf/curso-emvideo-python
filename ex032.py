74293bd527f8196fd2ad32848d2f8d87bfce96c8
carloscfm <kaikelmfabricio@gmail.com>
2020-09-06 09:45:11 -0300

    ex031,32,33

--- ex032.py
+++ ex032.py
@@ -1,4 +1,10 @@
-ano = int(input('Digite um ano : '))
+from datetime import datetime
+
+now = datetime.now()
+ano = int(input('Digite um ano, digite 0 para ano atual: '))
+
+if ano == 0:
+    ano = now.year
 
 if ano%400 == 0:
     print('o ano {} é bissexto'.format(ano))
@@ -6,3 +12,5 @@ elif ano%4 ==0 and ano % 100 >0:
     print('o ano {} é bissexto'.format(ano))
 else:
     print('o ano {} nao é bissexto '.format(ano))
+
+'''o guanabara apenas dividiu por 4 para achar o bissexto '''
--- ex033.py
+++ ex033.py
@@ -1,1 +1,18 @@
-
+n1 = float(input('Digite o 1o numero :'))
+n2 = float(input('Digite o segundo numero: '))
+n3 = float(input('diGite o 3o nUmero : '))
+numeros = [n1, n2, n3]
+numeros.sort()
+print('O maior desses 3 numeros é {} e o menor é {} '.format(numeros[2], numeros[0]))
+'''Modo guanabara'''
+menor = n1
+if n2 < n3 and n2 < n1:
+    menor = n2
+elif n3 < n2 and n3 < n1:
+    menor = n3
+maior = n1
+if n2 > n3 and n2 > n1:
+    maior = n2
+elif n3 > n2 and n3 > n1:
+    maior = n3
+print('O maior numero é {} e o menor é {}'.format(maior, menor))
